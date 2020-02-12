5a1781795a6233a053accd61dc624f79281e5e26
Ben Hickson <bzhickson@gmail.com>
2020-02-12 05:03:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -79,7 +79,7 @@ def game_hash
           slam_dunks:
         },
         {
-          player_name: '',
+          player_name: 'Bismack Biyombo',
           number: 0,
           shoe: 0,
           points: 0,
