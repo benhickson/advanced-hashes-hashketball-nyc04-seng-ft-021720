118958b20c6859c5f833b82ded02466238132dfe
Ben Hickson <bzhickson@gmail.com>
2020-02-12 04:59:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,8 +3,8 @@
 def game_hash
   the_hash = {
     home: {
-      team_name: '',
-      colors: ['',''],
+      team_name: 'Brooklyn Nets',
+      colors: ['Black','White'],
       players: [
         {
           player_name: '',
