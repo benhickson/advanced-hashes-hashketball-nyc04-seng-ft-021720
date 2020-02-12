5a8e66396a7e6b9a1b2f6881f10e61738dd13285
Ben Hickson <bzhickson@gmail.com>
2020-02-12 04:58:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,8 +4,41 @@ def game_hash
   the_hash = {
     home: {
       team_name: '',
-      colors: [],
+      colors: ['',''],
       players: [
+        {
+          player_name: '',
+          number: 0,
+          shoe: 0,
+          points: 0,
+          rebounds: 0,
+          assists: 0,
+          steals: 0,
+          blocks: 0,
+          slam_dunks:
+        },
+        {
+          player_name: '',
+          number: 0,
+          shoe: 0,
+          points: 0,
+          rebounds: 0,
+          assists: 0,
+          steals: 0,
+          blocks: 0,
+          slam_dunks:
+        },
+        {
+          player_name: '',
+          number: 0,
+          shoe: 0,
+          points: 0,
+          rebounds: 0,
+          assists: 0,
+          steals: 0,
+          blocks: 0,
+          slam_dunks:
+        },
         {
           player_name: '',
           number: 0,
