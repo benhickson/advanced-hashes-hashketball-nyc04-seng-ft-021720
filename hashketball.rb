47cb9b498aef5c6feeaf4b23c09e1ae6af032ea4
Ben Hickson <bzhickson@gmail.com>
2020-02-12 05:03:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -90,7 +90,7 @@ def game_hash
           slam_dunks:
         },
         {
-          player_name: '',
+          player_name: 'DeSagna Diop',
           number: 0,
           shoe: 0,
           points: 0,
