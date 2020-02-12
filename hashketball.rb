d78ca79e435b919679c6b2c92e47e0acd9571067
Ben Hickson <bzhickson@gmail.com>
2020-02-12 05:03:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -101,7 +101,7 @@ def game_hash
           slam_dunks:
         },
         {
-          player_name: '',
+          player_name: 'Ben Gordon',
           number: 0,
           shoe: 0,
           points: 0,
